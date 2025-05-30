e771c856cb27b7aff4e23bdac7b4980aa0491087
Ahmed Alian <ahmedalian400@gmail.com>
2025-05-30 19:36:22 +0300

    remove one config for jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,28 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1', // Matches @/...
-    '^@components/(.*)$': '<rootDir>/src/components/$1', // @components/...
-    '^@constants/(.*)$': '<rootDir>/src/constants/$1', // @constants/...
-    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1', // @hooks/...
-    '^@utils/(.*)$': '<rootDir>/src/utils/$1', // @utils/...
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-vector-icons|react-native-reanimated|react-native-gesture-handler|react-native-screens|react-native-safe-area-context|@react-navigation|react-native-track-player|react-native-mmkv|@kingfang007)/)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  testMatch: ['**/__tests__/**/*.(ts|tsx|js)', '**/*.(test|spec).(ts|tsx|js)'],
 };
