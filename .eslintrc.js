b8c402ca6b3f37f3b3dca6e5bc354e2f0a87b05d
Ahmed Alian <ahmedalian400@gmail.com>
2025-05-30 19:06:07 +0300

    remove rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
     ],
     // React Native specific rules
     'react-native/no-unused-styles': 'error',
-    'react-native/split-platform-components': 'error',
     'react-native/no-inline-styles': 'warn',
     'react-native/no-color-literals': 'warn',
     'react-native/no-raw-text': 'off',
